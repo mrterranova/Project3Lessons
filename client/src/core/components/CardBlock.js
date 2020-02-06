b4143f547bb63fda7ca257d9b325224436c3e123
Michal Terranova <michalterranova@mail.com>
2020-02-06 10:32:10 -0500

    component update

--- client/src/core/components/CardBlock.js
+++ client/src/core/components/CardBlock.js
@@ -4,16 +4,15 @@ import { Card } from 'react-bootstrap'
 
 const CardBlock = (props) => {
 
-  console.log(props)
-
   return (
-    <Card style={{ width: '12rem', height: '20rem' }}>
+    <Card key={props._id} style={{ width: '12rem', height: '20rem' }}>
       <Card.Img src="/images/scroll.png" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text>
           {props.keyTerms}
-          <p>{props.scriptures}</p>
+          <br />
+          {props.scriptures}
         </Card.Text>
       </Card.Body>
     </Card>
