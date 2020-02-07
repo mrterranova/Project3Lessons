e11d5f2fc975c64356ae23f5a3220f9ee406d353
Michal Terranova <michalterranova@mail.com>
2020-02-07 14:52:22 -0500

    change Hmac

--- models/user.js
+++ models/user.js
@@ -63,7 +63,7 @@ userSchema.methods = {
     encryptPassword : function(password) {
         if (!password) return ""
         try { 
-            return crypto.createHmac('sha1', this.salt)
+            return crypto.createHmac('sha256', this.salt)
             .update(password)
             .digest('hex')
         } catch (err) {
